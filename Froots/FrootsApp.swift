de2bab9e1a62e4ddd1d0e366da2ca91532e5a5bf
Utkarsh Dayal <utkarshdayal.17@gmail.com>
2021-05-12 12:21:47 +0530

    Initial Commit

---
+++ Froots/FrootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrootsApp.swift
+//  Froots
+//
+//  Created by Dayal, Utkarsh on 12/05/21.
+//
+
+import SwiftUI
+
+@main
+struct FrootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
